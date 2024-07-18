21d30276d184eb5829ae75d31ba738d32c599cba
mertcanoncul0 <mertcanoncul@icloud.com>
2024-07-18 19:35:07 +0300

    fixed html element

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,15 @@
-import { Header } from "./components/header"
-import { Search } from "./components/search"
-import { UserCard } from "./components/user-card"
+import { Header } from './components/header'
+import { Search } from './components/search'
+import { UserCard } from './components/user-card'
 
 export function App() {
   return (
     <>
       <Header />
-      <Search />
-      <UserCard />
+      <main>
+        <Search />
+        <UserCard />
+      </main>
     </>
   )
 }
