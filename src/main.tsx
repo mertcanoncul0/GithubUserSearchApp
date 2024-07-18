4524e8f3da2f11013d1f5e622a2a537044714507
mertcanoncul0 <mertcanoncul@icloud.com>
2024-07-18 19:23:15 +0300

    fixed responsive overflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { render } from "preact"
-import { ThemeProvider } from "./components/theme-provider.tsx"
-import { App } from "./app.tsx"
-import "./index.css"
+import { render } from 'preact'
+import { ThemeProvider } from './components/theme-provider.tsx'
+import { App } from './app.tsx'
+import './index.css'
 
 render(
   <ThemeProvider>
     <App />
   </ThemeProvider>,
-  document.getElementById("app")!
+  document.getElementById('app')!
 )
