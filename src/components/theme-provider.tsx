31380aaf20cbcb54736a4792feb943d357735719
mertcanoncul0 <mertcanoncul@icloud.com>
2024-07-18 20:40:20 +0300

    dark-light mode transition updated

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,12 +1,23 @@
-import React from "preact/compat"
+import React from 'preact/compat'
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const isDarkTheme = JSON.parse(
-    localStorage.getItem("theme-switcher") ||
+    localStorage.getItem('theme-switcher') ||
       '{"state":{"theme":true},"version":0}'
   ).state.theme
 
-  document.body.dataset.theme = isDarkTheme ? "dark" : "light"
+  document.body.dataset.theme = isDarkTheme ? 'dark' : 'light'
+
+  // Tüm elementlere geçiş stilini ekleyin
+  const style = document.createElement('style')
+  style.innerHTML = `
+    body, header h1, button, ul, ul span, input, input::placeholder, .search-form, p, .user-card, li, a, svg path {
+      transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+    }
+  `
+  setTimeout(() => {
+    document.head.appendChild(style)
+  }, 10)
 
   return <>{children}</>
 }
